1ad89797000e32b8422a8581d68d993dc8a38366
Jeremy Douglass <jeremydouglass@gmail.com>
2017-12-07 22:38:25 -0800

    add utils: pickle, exists

--- utils.py
+++ utils.py
@@ -1,12 +1,49 @@
+from __future__ import print_function
 import os
+import pickle
 
 # 'sketchPath()+'/input/index.html'
+def exists(filename, path=''):
+    if path == '':
+        path = sketchPath() + '/data/output/'
+    return os.path.exists(path+filename)
 
 def load_str(filename='panelcode.txt', path=''):
     if path == '':
         path = sketchPath() + '/data/input/'
     strings = loadStrings(path + filename)
     return strings
+
+def pickle_dump(obj, filename, path=''):
+    if path == '':
+        path = sketchPath() + '/data/output/'
+    try:
+        with open(path+filename, 'wb') as handle:
+            pickle.dump(obj, handle)
+    except EnvironmentError as err:
+        print(filename + ' not saved.')
+        print(err)
+        raise
+
+def pickle_load(filename, path=''):
+    if path == '':
+        path = sketchPath() + '/data/output/'
+    try:
+        with open(path+filename, 'rb') as handle:
+            return pickle.load(handle)
+    except EnvironmentError as err:
+        print(filename + ' not loaded.')
+        print(err)
+        raise
+
+def pickle_remove(filename, path=''):
+    if path == '':
+        path = sketchPath() + '/data/output/'
+    try:
+        os.remove(path+filename)
+    except OSError as err:
+        print(filename + ' not removed.')
+        print(err)
 
 def preview(filename='index.html', path=''):
     if path == '':
