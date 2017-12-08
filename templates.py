3d973f3bdf2934c47b258ecad0267fea43f5dd86
Jeremy Douglass <jeremydouglass@gmail.com>
2017-12-08 00:17:47 -0800

    templates: lint

--- templates.py
+++ templates.py
@@ -1,15 +1,16 @@
+"""Template wrapping functions for paneler, using Jinja2."""
+
 from jinja2 import Environment
 from jinja2.loaders import FileSystemLoader
-from jinja2 import Template
 
-spath = sketchPath()
-
-def load(filename, path = ''):
-    if path=='':
+def load(filename, path=''):
+    """Load template for rendering -- with sketch defaults."""
+    if path == '':
         path = sketchPath() + '/data/templates/'
     env = Environment(loader=FileSystemLoader(path))
     tmpl = env.get_template(filename)
     return tmpl
 
 def render(tmpl, code, title='Panelcode viewer'):
-    return tmpl.render(panelcode = code, pagetitle = title)
+    """Render code in template as content string"""
+    return tmpl.render(panelcode=code, pagetitle=title)
