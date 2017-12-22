a18e8a0c46a2b64f73bae8adad47613cb818341f
Jeremy Douglass <jeremydouglass@gmail.com>
2017-12-22 15:19:00 -0800

    panelcode.py: call fenced preparser

--- panelcode.py
+++ panelcode.py
@@ -6,7 +6,6 @@ import os
 import sys
 
 import preparse
-import parser
 import render
 
 
@@ -16,35 +15,16 @@ def decode(args):
     data_list = []
     for line in sys.stdin:
         data_list.append(line)
-    if not data_list:
+    if data_list:
+        if args.type == 'fence':
+            result_list = preparse.parse_fenced_to_html(data_list)
+            try:
+                sys.stdout.write('\n'.join(result_list))
+                return
+            except TypeError as err:
+                print err
+        sys.stdout.write('\n'.join(data_list))
         return
-    data_clean = preparse.decomment(data_list, '#')
-    data_str = "\n".join(data_clean)
-
-    if '```' in data_str:
-        graphs = data_str.split('```')
-        for idx, graph in enumerate(graphs):
-            if idx%2 == 1:
-                try:
-                    pcode_obj = parser.parse(graph, parser.root)
-                    html_lines = render.pobj_to_html5_ccs3_grid(pcode_obj)
-                    sys.stdout.write("\n".join(html_lines))
-                except parser.pp.ParseException:
-                    sys.stdout.write(graph)
-            else:
-                sys.stdout.write(graph)
-        return
-    else:
-        try:
-            pcode_obj = parser.parse(data_str, parser.root)
-            html_lines = render.pobj_to_html5_ccs3_grid(pcode_obj)
-            for line in html_lines:
-                sys.stdout.write(line)
-            return
-        except parser.pp.ParseException:
-            pass
-    sys.stdout.write('none')
-    sys.stdout.write("\n".join(data_list))
 
 
 if __name__ == "__main__":
