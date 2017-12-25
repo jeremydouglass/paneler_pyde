a33669297234a78e6ddd3f83d7d81989bf0f8cc4
Jeremy Douglass <jeremydouglass@gmail.com>
2017-12-25 07:51:29 -0800

    templates: add load2 with no Processing dependency for command line script

--- templates.py
+++ templates.py
@@ -5,6 +5,7 @@ from jinja2.loaders import FileSystemLoader
 
 import os
 
+
 def load(abspath='', filename='template.html'):
     """Load template for rendering -- with sketch defaults.
        Jinja2 is designed to work only within a relative
@@ -16,6 +17,22 @@ def load(abspath='', filename='template.html'):
     pathlist.append(sketchPath() + '/data/templates/')
     pathlist.append(sketchPath() + '/data/')
     pathlist.append(sketchPath())
+    env = Environment(loader=FileSystemLoader(pathlist))
+    tmpl = env.get_template(filename)
+    return tmpl
+
+
+def load2(abspath='', filename='template.html'):
+    """Load template for rendering -- with sketch defaults.
+       Jinja2 is designed to work only within a relative
+       list established by its Environment.
+    """
+    pathlist = []
+    if abspath:
+        pathlist.append(abspath)
+    pathlist.append(os.getcwd() + '/data/templates/')
+    pathlist.append(os.getcwd() + '/data/')
+    pathlist.append(os.getcwd())
     env = Environment(loader=FileSystemLoader(pathlist))
     tmpl = env.get_template(filename)
     return tmpl
