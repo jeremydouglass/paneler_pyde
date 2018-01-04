c605c7ed23c59ac6dcf85262c8df4b43c09cda69
Jeremy Douglass <jeremydouglass@gmail.com>
2018-01-03 22:17:18 -0800

    preparse: refactor sizer strings, move global sizer to page bottom, lint

--- preparse.py
+++ preparse.py
@@ -7,32 +7,12 @@ import parser
 import render
 import highlight
 
-def parse_fenced_to_html(data_list, mode='replace', panel='open', sizers=True, colorize=True):
-    """Parse panelcode only within markdown fenced code blocks.
-    Split a list of lines on fence open and close markers,
-    attempt to render code block contents as panelcode or pass through,
-    return a list of unchanged contents and possibly changed blocks.
-
-    Results can replace the code block ('replace') or come
-    before or after it ('pre' / 'post')
-    """
-    result_list = []
-    fences = re.compile(  # see mistune
-        r' *(`{3,}|~{3,})( *\S+ *)?\n'  # ```lang (removed)
-        r'([\s\S]+?\s*)'
-        r'(\1)(?: *\n+|$)'  # ```
-        )
-    data_fence_list = fences.split('\n'.join(data_list))
-    if colorize==True:
-        highlight_style = "<style>\n" + highlight.style_css() + "\n</style>"
-        result_list.append(highlight_style)
-    if sizers==True and len(data_fence_list)>1:
-        jquery_cdn = """<script
+JQUERY_SCRIPT_CDN = """<script
   src="https://code.jquery.com/jquery-2.2.4.min.js"
   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
   crossorigin="anonymous"></script>
  """
-        size_script ="""<script type="text/javascript">
+SIZER_SCRIPT = """<script type="text/javascript">
   $(document).ready(function(){
     // On load
     $('.gallery-size').change(function(){
@@ -48,7 +28,7 @@ def parse_fenced_to_html(data_list, mode='replace', panel='open', sizers=True, c
 })
 </script>
 """
-        size_selector_global = """Resize All: <select class="all-size">
+SIZER_GLOBAL = """<select class="all-size">
   <option value=""></option>
   <option value="default">default</option>
   <option value="small">small</option>
@@ -59,8 +39,7 @@ def parse_fenced_to_html(data_list, mode='replace', panel='open', sizers=True, c
 </select>
 <div style="clear: both;"></div>
 """
-        result_list.extend([jquery_cdn, size_script, size_selector_global])
-        size_selector = """<select class="gallery-size">
+SIZER_LOCAL = """<select class="gallery-size">
   <option value="">--size--</option>
   <option value="default">default</option>
   <option value="small">small</option>
@@ -70,6 +49,34 @@ def parse_fenced_to_html(data_list, mode='replace', panel='open', sizers=True, c
   <option value="micro2">micro2</option>
 </select>
 """
+
+
+def parse_fenced_to_html(data_list, mode='replace', reveal='',
+                         sizers=True, colorize=True):
+    """Parse panelcode only within markdown fenced code blocks.
+    Split a list of lines on fence open and close markers,
+    attempt to render code block contents as panelcode or pass through,
+    return a list of unchanged contents and possibly changed blocks.
+
+    Results can replace the code block ('replace') or come
+    before or after it ('pre' / 'post')
+    """
+    result_list = []
+    fences = re.compile(  # see mistune
+        r' *(`{3,}|~{3,})( *\S+ *)?\n'  # ```lang (removed)
+        r'([\s\S]+?\s*)'
+        r'(\1)(?: *\n+|$)'  # ```
+        )
+    data_fence_list = fences.split('\n'.join(data_list))
+    # General approach does not work -- Markdown processors strip style tags.
+    # Instead the css class styling information needs to already be available
+    #  via external stylsheets.
+    #
+    # if colorize==True:
+    #     highlight_style = "<style>\n" + highlight.style_css() + "\n</style>"
+    #     result_list.append(highlight_style)
+    if sizers and len(data_fence_list) > 1:
+        result_list.extend([JQUERY_SCRIPT_CDN, SIZER_SCRIPT])
     for idx, graph in enumerate(data_fence_list):
         graph = graph.replace('\n\n', '\n')
         if idx % 5 == 0:
@@ -77,18 +84,22 @@ def parse_fenced_to_html(data_list, mode='replace', panel='open', sizers=True, c
         if idx % 5 == 3:
             result = ''
             if colorize:
-                graph_out = '\n'+ str(highlight.style_string(graph)) +'\n'
+                graph_out = '\n' + str(highlight.style_string(graph)) + '\n'
             else:
-                graph_out = '\n```panelcode' + graph + '\n```\n' # data_fence_list[idx-2]
+                graph_out = '\n```panelcode' + graph + '\n```\n'
             try:
                 delims = ['#', '//']
                 graph_clean = ''.join(decomment(graph.split('\n'), delims))
                 pcode_obj = parser.parse(graph_clean, parser.root)
                 html_lines = render.pobj_to_html5_ccs3_grid(pcode_obj)
                 control_str = ''
-                if (sizers == True or 'control' in graph):
+                if sizers or 'control' in graph:
                     if 'nocontrol' not in graph:
-                        control_str = '<div style="clear: both;"></div><bdo dir="ltr"><details ' + panel + '><summary>panelcode</summary>\n' + size_selector + graph_out + '\n</details></bdo>\n'
+                        control_str = '<div style="clear: both;">'\
+                                    + '</div><bdo dir="ltr"><details '\
+                                    + reveal + '><summary>panelcode</summary>'\
+                                    + '\n' + SIZER_LOCAL + graph_out + '\n'\
+                                    + '</details></bdo>\n'
                 if mode == 'pre':
                     html_lines.insert(-1, control_str)
                     result += ''.join(html_lines)
@@ -97,10 +108,13 @@ def parse_fenced_to_html(data_list, mode='replace', panel='open', sizers=True, c
                     result += ''.join(html_lines)
                 elif mode == 'replace':
                     result = ''.join(html_lines)
-            except parser.pp.ParseException as err:
+            except parser.pp.ParseException:
                 result = graph_out
             result_list.append(result)
-    result_list.append('\n<p style="font-size:x-small"><em>panelcode: fence pre-processor</em></p>\n')
+    if sizers and len(data_fence_list) > 1:
+        result_list.append(SIZER_GLOBAL)
+    result_list.append('\n<p style="font-size:x-small">'
+                       + '<em>panelcode: fence pre-processor</em></p>\n')
     return result_list
 
 
