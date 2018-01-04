b2f79ff36aedc80f75b7e00d66e6f0285696953d
Jeremy Douglass <jeremydouglass@gmail.com>
2018-01-04 00:25:15 -0800

    highlight: lint

--- highlight.py
+++ highlight.py
@@ -1,86 +1,104 @@
-import re
+"""Syntax highlighting methods for Panelcode.
+   Custom lexer built with Pygments, custom style using Solarized colors.
+"""
 
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
 from pygments.lexer import bygroups, RegexLexer
-from pygments.lexers import load_lexer_from_file
-from pygments.token import *
+from pygments.token import Comment, Keyword, Literal, Name, Number, \
+                           Operator, Punctuation, String, Text
 from pygments.style import Style
 from pygments.styles import STYLE_MAP
 
-class PanelcodeLexer(RegexLexer): # PanelcodeLexer
+
+class PanelcodeLexer(RegexLexer):
+    """Lexer for Panelcode.
+    Token-based lexer for syntax highlighting (rather than parsing).
+    Does not support all Panelcode language features or do robust
+    error detection.
+    """
     name = 'Panelcode'
     aliases = ['panelcode', 'pc']
     filenames = ['*.panelcode', '*.pcode', '*.panelcode.txt', '*.pcode.txt']
     tokens = {
         'root': [
-            (r'\s+',   Text),                # whitespace
+            (r'\s+', Text),                # whitespace
             (r'//.*?$', Comment),
             (r'#.*?$', Comment),
-            (r'\{:*',  Punctuation, 'opts'),
+            (r'\{:*', Punctuation, 'opts'),
             (r'[\+\,\_\|\;\@]', Operator),
             (r'[\(\)]', Punctuation),
-
-            # (r'([a-zA-Z])', Punctuation, 'args'),
-            # (r'[0-9]', Number.Integer),
-            # (r'([a-zA-Z])', Punctuation, 'args'),
-
             (r'\.', Punctuation, 'dotopts'),
-            (r'(\.)([a-z])([0-9])', bygroups(Punctuation, Name.Attribute, Name.Property)),
-            (r'(\.)([a-z])(\-[0-9a-zA-Z\-]+)', bygroups(Punctuation, Name.Attribute, Name.Property)),
-            (r'(\.)([a-zA-Z][0-9a-zA-Z\-]+)', bygroups(Punctuation, Name.Attribute)),
-            (r'(\.)([a-zA-Z])', bygroups(Punctuation, Name.Attribute)),
-
-            (r'([crw])([0-9]+)', bygroups(Name.Attribute, Literal)),
-            (r'([biuxz])([0-9]+)', bygroups(Name.Attribute, Literal)),
-
+            (r'(\.)([a-z])([0-9])',
+                bygroups(Punctuation,
+                         Name.Attribute,
+                         Name.Property)),
+            (r'(\.)([a-z])(\-[0-9a-zA-Z\-]+)', bygroups(Punctuation,
+                                                        Name.Attribute,
+                                                        Name.Property)),
+            (r'(\.)([a-zA-Z][0-9a-zA-Z\-]+)', bygroups(Punctuation,
+                                                       Name.Attribute)),
+            (r'(\.)([a-zA-Z])', bygroups(Punctuation,
+                                         Name.Attribute)),
+            (r'([crw])([0-9]+)', bygroups(Name.Attribute,
+                                          Literal)),
+            (r'([biuxz])([0-9]+)', bygroups(Name.Attribute,
+                                            Literal)),
             (r'[a-zA-Z\-][0-9a-zA-Z\-]*', Name.Attribute),
-            # (r'[0-9][0-9]', Error),
             (r'[0-9]', Number.Integer),
         ],
-        'dotopts' : [
+        'dotopts': [
             (r'[\+\,\_\|\;\@]', Operator, '#pop'),
             (r'\s+', Text, '#pop'),
-
-            (r'([crw])([0-9]+)', bygroups(Name.Attribute, Literal)),
-            (r'([biuxz])([0-9]+)', bygroups(Name.Attribute, Literal)),
-
+            (r'([crw])([0-9]+)', bygroups(Name.Attribute,
+                                          Literal)),
+            (r'([biuxz])([0-9]+)', bygroups(Name.Attribute,
+                                            Literal)),
             (r'(\.)', Punctuation),
             (r'[^\s\.]+', Name.Attribute)
         ],
-        'opts' : [
+        'opts': [
             (r'(\.)', Punctuation),
             (r'\}', Punctuation, '#pop'),
             (r'\s+', Text),
-
-            (r'([crw])([0-9]+)', bygroups(Name.Attribute, Literal)),
-            (r'([biuxz])([0-9]+)', bygroups(Name.Attribute, Literal)),
-
-            (r'(\w+)(\s?)([\:=])(\s?)',
-             bygroups(Name.Attribute, Text, Punctuation, Text), 'optarg'),
+            (r'([crw])([0-9]+)', bygroups(Name.Attribute,
+                                          Literal)),
+            (r'([biuxz])([0-9]+)', bygroups(Name.Attribute,
+                                            Literal)),
+            (r'(\w+)(\s?)([\:=])(\s?)', bygroups(Name.Attribute,
+                                                 Text,
+                                                 Punctuation,
+                                                 Text), 'optarg'),
             (r'[^\}\s]+', Name.Attribute)
         ],
-        'optarg' : [
-            (r'(\')([^\']*)(\')', bygroups(Punctuation, String.Single, Punctuation), '#pop'),
-            (r'(\")([^\']*)(\")', bygroups(Punctuation, String.Double, Punctuation), '#pop'),
+        'optarg': [
+            (r'(\')([^\']*)(\')', bygroups(Punctuation,
+                                           String.Single,
+                                           Punctuation), '#pop'),
+            (r'(\")([^\']*)(\")', bygroups(Punctuation,
+                                           String.Double,
+                                           Punctuation), '#pop'),
             (r'[0-9]+', Number.Integer, '#pop')
         ]
     }
 
+
 class SolarizedStyle(Style):
-    """Maps Solarized colors to Panelcode pygments tokens."""
+    """Style map for lexed Panelcode.
+    Maps Solarized colors to Pygments token class names as CSS definitions.
+    """
 
     mode = 'light'
     default_style = ""
-    
-    base03 =  '#002b36' # dark: background
-    base02 =  '#073642' # dark: bg highlights
-    base01 =  '#586e75' # dark: comment       | light: emphasis
-    base00 =  '#657b83' #                     | light: content
-    base0 =   '#839496' # dark: content
-    base1 =   '#93a1a1' # dark: emphasis      | light: comment
-    base2 =   '#eee8d5' #                     | light: bg highlights
-    base3 =   '#fdf6e3' #                     | light: background
+
+    base03 =  '#002b36'  # dark: background
+    base02 =  '#073642'  # dark: bg highlights
+    base01 =  '#586e75'  # dark: comment       -- | light: emphasis
+    base00 =  '#657b83'  # --                  -- | light: content
+    base0 =   '#839496'  # dark: content
+    base1 =   '#93a1a1'  # dark: emphasis      -- | light: comment
+    base2 =   '#eee8d5'  # --                  -- | light: bg highlights
+    base3 =   '#fdf6e3'  # --                  -- | light: background
     yellow =  '#b58900'
     orange =  '#cb4b16'
     red =     '#dc322f'
@@ -107,7 +125,7 @@ class SolarizedStyle(Style):
         Operator:       'bold ' + magenta,    # Matches @ ; | _ , + - and = :
         Punctuation:    'bold ' + content,
         Comment:        'italic ' + comment,
-        Comment.Single: 'italic ' + comment, # Matches // or ## and on
+        Comment.Single: 'italic ' + comment,  # Matches // or # to end-of-line
         Number:         'bold ' + blue,
         Name:           'bold ' + violet,
         Literal:         violet,
@@ -134,6 +152,12 @@ def all_styles(code, outpath, lexer=PanelcodeLexer(),
 
 def style_string(code, lexer=PanelcodeLexer(), style=SolarizedStyle,
                  full=False):
+    """Render plaintext Panelcode as syntax-highlighting HTML.
+    Code is returned as tokens tagged with <span class="sometokenID">.
+    Uses a custom Panelcode lexer and a color style that targets
+    Panelcode token types and distributions. The style argument can be
+    a string naming any built-in style, e.g. 'paraiso-dark'.
+    """
     formatter = HtmlFormatter(style=style)
     formatter.full = full
     html_str = highlight(code, lexer, formatter)
