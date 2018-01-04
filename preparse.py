ecf2ec5aae678bc02ddcb385d2709d51f899d5b8
Jeremy Douglass <jeremydouglass@gmail.com>
2018-01-03 22:44:30 -0800

    preparser: template-based gallery console generation (refactor)

--- preparse.py
+++ preparse.py
@@ -6,6 +6,7 @@ import re
 import parser
 import render
 import highlight
+import templates
 
 JQUERY_SCRIPT_CDN = """<script
   src="https://code.jquery.com/jquery-2.2.4.min.js"
@@ -28,31 +29,27 @@ SIZER_SCRIPT = """<script type="text/javascript">
 })
 </script>
 """
-SIZER_GLOBAL = """<select class="all-size">
-  <option value=""></option>
-  <option value="default">default</option>
-  <option value="small">small</option>
-  <option value="thumb">thumb</option>
-  <option value="mini">mini</option>
-  <option value="micro">micro</option>
-  <option value="micro2">micro2</option>
-</select>
-<div style="clear: both;"></div>
-"""
-SIZER_LOCAL = """<select class="gallery-size">
-  <option value="">--size--</option>
-  <option value="default">default</option>
-  <option value="small">small</option>
-  <option value="thumb">thumb</option>
-  <option value="mini">mini</option>
-  <option value="micro">micro</option>
-  <option value="micro2">micro2</option>
-</select>
-"""
+
+
+def console_html(size_list='', content='', summary='panelcode',
+                 css_class='gallery-size', reveal='open'):
+    """Render a console area for a gallery. Includes:
+    1. a code view (syntax highlighting)
+    2. resizing of gallery layouts
+    3. show / hide console contents
+    """
+    if not size_list:
+        size_list = ['', 'default', 'small', 'thumb', 'mini', 'micro2']
+    template = 'console.html'
+    tmpl = templates.load2(filename=template)
+    html_str = tmpl.render(summary=summary, option_list=size_list,
+                           css_class=css_class, content=content,
+                           reveal=reveal).encode('utf-8')
+    return html_str
 
 
 def parse_fenced_to_html(data_list, mode='replace', reveal='',
-                         sizers=True, colorize=True):
+                         consoles=True, colorize=True):
     """Parse panelcode only within markdown fenced code blocks.
     Split a list of lines on fence open and close markers,
     attempt to render code block contents as panelcode or pass through,
@@ -75,7 +72,7 @@ def parse_fenced_to_html(data_list, mode='replace', reveal='',
     # if colorize==True:
     #     highlight_style = "<style>\n" + highlight.style_css() + "\n</style>"
     #     result_list.append(highlight_style)
-    if sizers and len(data_fence_list) > 1:
+    if consoles and len(data_fence_list) > 1:
         result_list.extend([JQUERY_SCRIPT_CDN, SIZER_SCRIPT])
     for idx, graph in enumerate(data_fence_list):
         graph = graph.replace('\n\n', '\n')
@@ -87,21 +84,20 @@ def parse_fenced_to_html(data_list, mode='replace', reveal='',
                 graph_out = '\n' + str(highlight.style_string(graph)) + '\n'
             else:
                 graph_out = '\n```panelcode' + graph + '\n```\n'
+                # ... or use data_fence_list[idx-2] -- catches ~~~ etc.
             try:
                 delims = ['#', '//']
                 graph_clean = ''.join(decomment(graph.split('\n'), delims))
                 pcode_obj = parser.parse(graph_clean, parser.root)
                 html_lines = render.pobj_to_html5_ccs3_grid(pcode_obj)
-                control_str = ''
-                if sizers or 'control' in graph:
-                    if 'nocontrol' not in graph:
-                        control_str = '<div style="clear: both;">'\
-                                    + '</div><bdo dir="ltr"><details '\
-                                    + reveal + '><summary>panelcode</summary>'\
-                                    + '\n' + SIZER_LOCAL + graph_out + '\n'\
-                                    + '</details></bdo>\n'
+                console_str = ''
+                if consoles or 'console' in graph:
+                    if 'noconsole' not in graph:
+                        console_str = console_html(content=graph_out,
+                                                   css_class='gallery-size',
+                                                   reveal=reveal)
                 if mode == 'pre':
-                    html_lines.insert(-1, control_str)
+                    html_lines.insert(-1, console_str)
                     result += ''.join(html_lines)
                 elif mode == 'post':
                     result += '\n' + graph_out + '\n'
@@ -111,8 +107,12 @@ def parse_fenced_to_html(data_list, mode='replace', reveal='',
             except parser.pp.ParseException:
                 result = graph_out
             result_list.append(result)
-    if sizers and len(data_fence_list) > 1:
-        result_list.append(SIZER_GLOBAL)
+    if consoles and len(data_fence_list) > 1:
+        console_str = console_html(content='',
+                                   summary='Resize all galleries: ',
+                                   css_class='all-size',
+                                   reveal=reveal)
+        result_list.append(console_str)
     result_list.append('\n<p style="font-size:x-small">'
                        + '<em>panelcode: fence pre-processor</em></p>\n')
     return result_list
