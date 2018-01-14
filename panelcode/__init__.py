e0a471d1ff0325f5af9aaca8b20d2946e3fe7ace
Jeremy Douglass <jeremydouglass@gmail.com>
2018-01-14 11:19:27 -0800

    libs: give libs precedence over local installs

--- panelcode/__init__.py
+++ panelcode/__init__.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "libs"))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "libs"))
 
 import batcher
 import jinja2
